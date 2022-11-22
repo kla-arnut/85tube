29e7616a7477dd2e52a3e8931c4cecb9506a5f3a
Arnut Klamhui <kla.arnut@gmail.com>
2022-11-22 11:51:44 +0700

    update

--- convert_old_video_to_m3u8.py
+++ convert_old_video_to_m3u8.py
@@ -25,9 +25,9 @@ for dir in allDir:
     # get video preview
     if os.path.exists(os.path.join(dir,videoID+'_preview.mp4')):
         videoPreview = os.path.join(dir,videoID+'_preview.mp4')
-
+    
+    
     # convert video from mp4 to m3u8
-    videoPathRegis = 'videos'
     if videoFile != None:
         videoName = os.path.basename(os.path.splitext(videoFile)[0])
         m3u8File = os.path.join(dir,videoName+'.m3u8')
@@ -36,7 +36,12 @@ for dir in allDir:
         print('output file:',m3u8File)
         inputFile = ffmpeg.input(videoFile, f='mp4')
         outputFile = ffmpeg.output(inputFile, m3u8File, format='hls', start_number=0, hls_time=5, hls_list_size=0)
-        ffmpeg.run(outputFile)
+        # check if m3u8 is exists
+        if not os.path.exists(m3u8File):
+            ffmpeg.run(outputFile)
+        else:
+            print('is exists',m3u8File)
+        
 
     if videoPreview != None:
         videoPreviewName = os.path.basename(os.path.splitext(videoPreview)[0])
@@ -46,7 +51,12 @@ for dir in allDir:
         print('output file:',m3u8PreviewFile)
         inputFile = ffmpeg.input(videoPreview, f='mp4')
         outputFile = ffmpeg.output(inputFile, m3u8PreviewFile, format='hls', start_number=0, hls_time=5, hls_list_size=0)
-        ffmpeg.run(outputFile)
+        # check if m3u8 is exists
+        if not os.path.exists(m3u8PreviewFile):
+            ffmpeg.run(outputFile)
+        else:
+            print('is exists',m3u8PreviewFile)
+        
     
     print('successful',count,'of',len(allDir))
     count = count+1
